0e1ce53c36ad5acbda49bbabf237286efad9a4a4
Annie <devannie777@gmail.com>
2022-05-10 11:04:40 +0100

    Mapping over notes array

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Note from './components/Note'
+import notes from './notes'
 
+const noteItem = notes.map(note => (
+  <Note
+    key={note.key}
+    title={note.title}
+    content={note.content}
+  />
+))
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {noteItem}
       <Footer />
     </div>
   )
